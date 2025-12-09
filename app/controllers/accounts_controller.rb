a8fceead4d359bb5bf28dae7c03654ed57ac421d
Yeiner Carvajal Quina <ycarvajalq@gmail.com>
2025-12-08 23:25:20 -0500

    add Accounts controller and install simple_form gem

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,50 @@
 class AccountsController < ApplicationController
+  before_action :set_accout, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @accounts = Account.all
+  end
+
+  def show
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to accounts_path, notice: "Account was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to accounts_path, notice: "Account was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to accounts_path, notice: "Account was successfully destroyed."
+  end
+
+  private
+
+  def set_accout
+    @account = Account.find(params[:id])
+  end
+
+  def account_params
+    params.require(:account).permit(:number, :name, :nature, :account_type)
+  end
 end
